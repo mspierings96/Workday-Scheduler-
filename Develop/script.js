2f83599173eefd87f05031865577fa6a3a438927
Your Name <you@example.com>
2021-04-03 18:45:15 -0500

    changes to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,53 @@ console.log(currentDay)
 $("#currentDay").text(currentDay)
 
 
-// The next requirement of the project: "WHEN I scroll down THEN I am presented with time blocks for standard business hours"
-
 var hour=moment().hour()
 console.log(hour)
+
+
+for (var i = 8; i < 18; i++) {
+
+
+var row = $('<div>')
+var time = $('<span>')
+var textArea = $('<textarea>')
+var savebutton = $('<button>')
+
+
+    savebutton.addClass('save-btn')
+    savebutton.text('Save')
+
+    var timeText = ""
+    if(i > 12) {
+        timeText = i - 12 + " PM"
+        textArea.attr('id', i - 12)
+        savebutton.attr('name',  i - 12 )
+        console.log('IS THIS in the local storage from before ????', localStorage.getItem(i -12 ))
+        if(localStorage.getItem(i -12 )){
+            textArea.text(localStorage.getItem(i -12 ))
+        }
+
+    } else {
+        timeText = i + ' am '
+        textArea.attr('id', i)
+        savebutton.attr('name',  i)
+        console.log('IS THIS in the local storage from before ????', localStorage.getItem(i))
+        if(localStorage.getItem(i)) {
+            textArea.text(localStorage.getItem(i))
+        }
+    }
+
+    time.text(timeText)
+
+
+row.append(time,textArea,savebutton)
+$('.container').append(row)
+}
+
+
+$('.save-btn').on('click', function() {
+    console.log("The button hour they clicked", $(this).attr('name'))
+  
+    console.log('Text area for that hourrrr', $('#' +  $(this).attr('name')).val())
+    localStorage.setItem($(this).attr('name'), $('#' +  $(this).attr('name')).val())
+})
